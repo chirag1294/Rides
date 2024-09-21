8ec87d6936b2fe516179772b82c7b376bb179136
chirag1294 <anghan.chirag1212@gmail.com>
2024-09-21 11:31:51 -0400

    Initial Commit

---
+++ Rides/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rides
+//
+//  Created by Chirag Anghan on 2024-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Chirag Anghan on 2024-09-21.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
